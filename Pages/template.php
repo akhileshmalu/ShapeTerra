6a088bc5345dad29c9072434e545d11977b6a589
Blake Finn <bfinn@email.sc.edu>
2016-10-13 12:55:55 -0400

    Added resources for menu

---
+++ Pages/template.php
@@ -0,0 +1,17 @@
+<?php
+	//Include Header
+	require_once("../Resources/Includes/header.php");
+?>
+</head>
+<body>
+<?php
+	// Include Menu and Top Bar
+	require_once("../Resources/Includes/menu.php");
+?>
+
+<!-- Main Content goes here -->
+
+<?php
+	//Include Footer
+	require_once("../Resources/Includes/footer.php");
+?>
--- Resources/Includes/header.php
+++ Resources/Includes/header.php
@@ -10,9 +10,9 @@
   <link rel="stylesheet" href="../Resources/Library/css/bootstrap.css">
   <link rel="stylesheet" href="../Resources/Library/css/bootstrap-theme.css">
   <link rel="stylesheet" href="../Resources/Library/css/animate.css">
-  <link href="css/layout.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="../Resources/Library/css/general.css">
+
   
 
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-
 
