6b747d754190b1e8b35aba32ae387fcec3144738
Blake Finn <bfinn@email.sc.edu>
2016-11-01 22:06:52 -0400

    Added tabs to blueprint page. Created templateTab page

--- Pages/adday.php
+++ Pages/adday.php
@@ -1,9 +1,8 @@
 <?php
-require_once("../Resources/Includes/header.php");
+
 /*
  * This Page controls addition of Academic Year module.
  */
-
 
 session_start();
 $error = array();
@@ -13,8 +12,6 @@ $ouname[]="";
 require_once ("../Resources/Includes/connect.php");
 $sql = "Select * from Hierarchy";
 $result = $mysqli->query($sql);
-
-
 
 
 if(isset($_POST['submit'])) {
@@ -45,7 +42,6 @@ if(isset($_POST['submit'])) {
     }
 }
 
-
 /*
  * Function to obtain String from ID and ID from String.
  */
@@ -57,73 +53,92 @@ function stringtoid ($string1, $string2){
     $id = ($id1*100)+$id2;
     return $id;
 }
+
 function idtostring ($id){
     $id2= $id %100;
     $id1= intval($id/100);
     $string = "AY20".$id1."-20".$id2;
     return $string;
 }
-?>
 
+require_once("../Resources/Includes/header.php");
 
-<link href="../Resources/Library/css/bootstrap-datetimepicker.css" rel="stylesheet" type="text/css" />
-<link href="Css/blueprint.css" rel="stylesheet" type="text/css" />
-<body>
-<?php
 // Include Menu and Top Bar
 require_once("../Resources/Includes/menu.php");
 ?>
 
-<div class="hr"></div>
-<div id="main-content" class="col-lg-10 col-xs-8">
-    <div class="col-xs-12">
-        <h1 id="title" class="col-lg-5 col-xs-8">Academic BluePrint</h1>
-    </div>
-    <div class="col-xs-4" id="table-container">
-    <form action ="" method="POST">
-            <label for="datetimepicker1">Please Select Academic Year Start date :</label>
-            <div class="form-group">
-                <div class='input-group date'  id='datetimepicker1'>
-                    <input type='text' name="startdate"  class="form-control" required/>
-                    <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
-                </div>
-            </div>
-            <label for="datetimepicker2">Please Select Academic Year End date :</label>
-            <div class="form-group">
-                <div class='input-group date' id='datetimepicker2'>
-                    <input type='text' name="enddate" class="form-control" required />
-                    <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
-                </div>
-            </div>
-            <label for="datetimepicker3">Please Select Date Census :</label>
-            <div class="form-group">
-                <div class='input-group date' id='datetimepicker3'>
-                    <input type='text' name="censusdate" class="form-control" required/>
-                    <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
-                </div>
-            </div>
-            <label for="ouname">Please Select Organization Unit(s)</label>
-            <?php while($row = $result->fetch_array(MYSQLI_NUM)): { ?>
-                <div class="checkbox" id="ouname">
-                <label><input type="checkbox" name="ou_name[]" value="<?php echo $row[0]; ?>"><?php echo $row[1]; ?></label>
-            </div>
-            <?php } endwhile;?>
-            <?php if(isset($_POST['submit'])) { ?>
-                <div class="alert alert-warning">
-                    <?php foreach ($error as $value)echo $value; ?>
-                </div>
-            <?php } ?>
-            <input type="submit" name="submit" value="Submit" class="btn-primary">
+<link href="Css/templateTabs.css" rel="stylesheet" type="text/css" />
+<link href="../Resources/Library/css/bootstrap-datetimepicker.css" rel="stylesheet" type="text/css" />
 
-    </form>
+<div class="hr"></div>
+    <div id="main-content" class="col-xs-10">
+        <h1 id="title">Academic BluePrint</h1>
+ 
+        <ul id="tabs" class="nav nav-pills" id="menu-secondary" role="tablist">
+            <li class="active"><a href="#add">Tab 1</a></li>
+            <li><a href="#view">Tab 2</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active fade in" id="add">
+                <div class="col-xs-4" id="table-container">
+                    <form action ="" method="POST">
+                    <label for="datetimepicker1">Please Select Academic Year Start date :</label>
+                    <div class="form-group">
+                        <div class='input-group date'  id='datetimepicker1'>
+                            <input type='text' name="startdate"  class="form-control" required/>
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-calendar"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <label for="datetimepicker2">Please Select Academic Year End date :</label>
+                    <div class="form-group">
+                        <div class='input-group date' id='datetimepicker2'>
+                            <input type='text' name="enddate" class="form-control" required />
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-calendar"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <label for="datetimepicker3">Please Select Date Census :</label>
+                    <div class="form-group">
+                        <div class='input-group date' id='datetimepicker3'>
+                            <input type='text' name="censusdate" class="form-control" required/>
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-calendar"></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        <div role="tabpanel" class="tab-pane fade " id="view">
+            <label for="ouname">Please Select Organization Unit(s)</label>
+                <?php while($row = $result->fetch_array(MYSQLI_NUM)): { ?>
+                    <div class="checkbox" id="ouname">
+                    <label><input type="checkbox" name="ou_name[]" value="<?php echo $row[0]; ?>"><?php echo $row[1]; ?></label>
+                </div>
+                <?php } endwhile;?>
+                <?php if(isset($_POST['submit'])) { ?>
+                    <div class="alert alert-warning">
+                        <?php foreach ($error as $value)echo $value; ?>
+                    </div>
+                <?php } ?>
+                <input type="submit" name="submit" value="Submit" class="btn-primary pull-left">
+                </form>
+        </div>
+    </div>
 </div>
-</div>
+    </div>
+ </form>
+
+
+
+
+
+
 <?php
 //Include Footer
 require_once("../Resources/Includes/footer.php");
